83829fd977be149538b857dbf3310052aa478005
mfwass <michael@mfwass.com>
2017-09-04 16:13:44 -0400

    aespyd: Fix darwin build command

--- aespyd.sh
+++ aespyd.sh
@@ -1,2 +1,2 @@
 g++ -I/usr/include/python2.7 aes.cxx -lpython2.7 -lcrypto \
-    -shared -fPIC -o aes.so -DBUILD_PYD
+    -shared -fPIC -o aes.so -DBUILD_PYD -L/usr/local/opt/openssl/lib -I/usr/local/opt/openssl/include
