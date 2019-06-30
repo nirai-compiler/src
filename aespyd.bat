2448631ef5a36197befa53e68123ed2d9f4a50f6
John Cote <jwcotejr@users.noreply.github.com>
2019-06-30 18:54:20 -0400

    Update aespyd.bat

--- aespyd.bat
+++ aespyd.bat
@@ -3,11 +3,11 @@
 rem Compiles aes.cxx to a .pyd file.
 
 set CMD=cl /c /nologo /EHsc /DBUILD_PYD /I../panda3d/thirdparty/win-python/include
-set CMD=%CMD% /I../panda3d/thirdparty/win-libs-vc10/openssl/include aes.cxx
+set CMD=%CMD% /I../panda3d/thirdparty/win-libs-vc14/openssl/include aes.cxx
 %CMD%
 
 set CMD=link /DLL /LIBPATH:"../panda3d/thirdparty/win-python/libs"
-set CMD=%CMD% /LIBPATH:"../panda3d/thirdparty/win-libs-vc10/openssl/lib" /OUT:aes.pyd aes.obj libpandaeay.lib
+set CMD=%CMD% /LIBPATH:"../panda3d/thirdparty/win-libs-vc14/openssl/lib" /OUT:aes.pyd aes.obj libeay32.lib ssleay32.lib
 set CMD=%CMD% advapi32.lib gdi32.lib user32.lib crypt32.lib
 %CMD%
 
